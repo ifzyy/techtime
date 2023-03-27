b812d8169cdfad54b69c98c47658af732fa8ca85
Johnson Emmanuel <johnsonnifemi8@gmail.com>
2023-03-27 02:19:46 +0100

    complete app

--- src/components/Community.js
+++ src/components/Community.js
@@ -5,14 +5,14 @@ import '../styles/hero.css'
 import '../styles/community.css'
 const Community = () => {
   useEffect(() => {
-    AOS.init({ duration: 2000 })
+    AOS.init({ duration: 4000 })
   }, [])
   return (
     <div className="community">
       <p data-aos="flip-down">join our community</p>
       <h2 data-aos="zoom-in-up">are you  ready to connect with the future talent of the tech world</h2>
       <p className="p-again" data-aos="zoom-in-up">meet up with other techstars and grow together</p>
-      <div className='world-image' data-aos="zoom-in-up" data-aos-duration="3000">
+      <div className='world-image' data-aos="zoom-in-up" data-aos-duration="5000">
         <img src="images/community.png" alt=""  className='desk-community'/>
         <img src="images/mobile-community.png" alt="" className='mobile-community'/>
       </div>
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -5,7 +5,7 @@ import 'aos/dist/aos.css'
 import '../styles/hero.css'
 const Testimonial = () => {
     useEffect(() => {
-        AOS.init({ duration: 2000 })
+        AOS.init({ duration: 3000 })
     }, [])
     return (
         <div className="testimonial">
